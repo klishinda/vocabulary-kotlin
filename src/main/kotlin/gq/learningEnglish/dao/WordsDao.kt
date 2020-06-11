52ae12ec9a6a92cca158f848e7244ab6d2399645
tronix <klishin.d.a@yandex.ru>
2020-06-11 10:12:42 +0300

    redone quiz controller. some fixes with "insert... returning" statements

--- src/main/kotlin/gq/learningEnglish/dao/WordsDao.kt
+++ src/main/kotlin/gq/learningEnglish/dao/WordsDao.kt
@@ -1,6 +1,7 @@
 package gq.learningEnglish.dao
 
 import gq.learningEnglish.common.JdbcDao
+import gq.learningEnglish.common.namedQuery
 import gq.learningEnglish.common.query
 import gq.learningEnglish.model.Word
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
@@ -11,8 +12,8 @@ class WordsDao(private val jdbc: JdbcDao) {
 
     fun getWord(id: Long): Word = jdbc.query(GET_WORD, id)
 
-    fun addWord(word: Word) : Long = jdbc.namedUpdate(ADD_WORD, word.sqlParams).toLong()
+    fun addWord(word: Word) : Long = jdbc.namedQuery(ADD_WORD, word.sqlParams)
 }
 
-private const val ADD_WORD = "insert into words(id, word, language, description, part_of_speech) values (default, :word, :language, :description, :partOfSpeech)"
+private const val ADD_WORD = "insert into words(id, word, language, description, part_of_speech) values (default, :word, :language, :description, :partOfSpeech) returning id"
 private const val GET_WORD = "select w.id, w.word, w.language, w.description, w.part_of_speech from words w where w.id = ?"
