fdf3a75956f952bbcd0978c7bf44025aa0dc4dec
tronix <klishin.d.a@yandex.ru>
2020-08-25 10:48:44 +0300

    added swagger

--- src/main/kotlin/gq/learningEnglish/configuration/Config.kt
+++ src/main/kotlin/gq/learningEnglish/configuration/Config.kt
@@ -5,8 +5,14 @@ import gq.learningEnglish.common.JdbcDao
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
+@EnableSwagger2
 open class Config {
 
     @Bean
@@ -16,4 +22,13 @@ open class Config {
     @Bean
     @Autowired
     open fun postgreDataSource(configuration: PostgreConfiguration): HikariDataSource = HikariDataSource(configuration)
+
+    @Bean
+    open fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build()
+    }
 }
