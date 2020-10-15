23c5ad8f29b3b257b2bfb21f31093f7ce6fe109e
tronix <klishin.d.a@yandex.ru>
2020-10-15 16:43:18 +0300

    Added full logging instead of "println"

--- src/main/kotlin/gq/learningEnglish/controller/VocabularyController.kt
+++ src/main/kotlin/gq/learningEnglish/controller/VocabularyController.kt
@@ -1,5 +1,6 @@
 package gq.learningEnglish.controller
 
+import gq.learningEnglish.common.infrastructure.interfaces.Logger
 import gq.learningEnglish.model.enums.AvailableLanguages.*
 import gq.learningEnglish.model.Word
 import gq.learningEnglish.service.VocabularyService
@@ -7,33 +8,33 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/vocabulary")
-class VocabularyController(private val vocabularyService: VocabularyService) {
+class VocabularyController(private val vocabularyService: VocabularyService) : Logger {
 
-    @PostMapping("/start")
-    fun start() {
-        println("Start")
+    @PostMapping("/test")
+    fun testFunction() {
+        log.info("Start")
         val rusWord = Word(name = "СНЕГ", language = RUSSIAN, partOfSpeech = 101)
         val engWord = Word(name = "SNOW", language = ENGLISH, partOfSpeech = 101)
         val addedIdRus: Long = vocabularyService.addWord(rusWord, USER_NAME)
         val addedIdEng: Long = vocabularyService.addWord(engWord, USER_NAME)
         val addedVocabularyId: Long = vocabularyService.addWordPair(addedIdRus, addedIdEng, USER_NAME)
-        println("$addedVocabularyId $addedIdRus $addedIdEng")
+        log.info("vocabulary id = $addedVocabularyId, russian word id = $addedIdRus, english word id = $addedIdEng")
     }
 
     @PostMapping("/add-word")
     fun addWord(@RequestBody word: Word, @RequestParam user: String) =
-        vocabularyService.addWord(word, user).also { println(it) }
+        vocabularyService.addWord(word, user).also { log.info("new id $it") }
 
     @PostMapping("/add-pair")
     fun addWordPair(@RequestParam firstId: Long, @RequestParam secondId: Long, @RequestParam user: String) =
-        vocabularyService.addWordPair(firstId, secondId, user).also { println(it) }
+        vocabularyService.addWordPair(firstId, secondId, user).also { log.info("new id $it") }
 
     @GetMapping("/translate")
     fun getTranslate(@RequestParam wordId: Long, @RequestParam user: String) =
-        vocabularyService.getTranslate(wordId, user).also { println(it) }
+        vocabularyService.getTranslate(wordId, user).also { log.info(it.toString()) }
 
     @GetMapping("/unused-words")
-    fun getUnusedWords() = vocabularyService.getUnusedWords().also { println(it) }
+    fun getUnusedWords() = vocabularyService.getUnusedWords().also { log.info(it.toString()) }
 }
 
 private const val USER_NAME = "Tronix"
