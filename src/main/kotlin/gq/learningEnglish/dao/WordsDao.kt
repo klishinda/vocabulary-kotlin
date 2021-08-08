e2bb9dedb174518b134626d8fd0d5b48f90a256d
tronix <klishin.d.a@yandex.ru>
2021-08-08 11:32:58 +0300

    1. added new functionality 2. improving sql (when added word exists)

--- src/main/kotlin/gq/learningEnglish/dao/WordsDao.kt
+++ src/main/kotlin/gq/learningEnglish/dao/WordsDao.kt
@@ -11,11 +11,13 @@ class WordsDao(private val jdbc: JdbcDao) {
 
     fun getWord(id: Long, userId: Long): Word = jdbc.query(GET_WORD, id, userId)
 
-    fun addWord(word: Word, userId: Long): Long = jdbc.namedQuery(ADD_WORD, word.sqlParams.addValue("userId", userId))
+    fun addWord(word: Word, userId: Long): Word = jdbc.namedQuery(ADD_WORD, word.sqlParams.addValue("userId", userId))
 }
 
 private const val ADD_WORD =
     """insert into words(id, user_id, word, language, description, part_of_speech)
-    values (default, :userId, upper(:word), :language, :description, :part_of_speech) returning id"""
+        values (default, :userId, upper(:word), :language, :description, :part_of_speech)
+        on conflict (user_id, word, language, description) do update set word = excluded.word
+    returning *"""
 private const val GET_WORD =
     "select w.id, w.word, w.language, w.description, w.part_of_speech from words w where w.id = ? and user_id = ?"
